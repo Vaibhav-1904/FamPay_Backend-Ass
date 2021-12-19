c973f22854aecc156098d6e265879bfb1f1450d1
unknown <vaibhav6644@gmail.com>
2021-12-19 18:09:26 +0530

    Search Query Results are now getting Stored in Database, added ChannelTitle column in the Model

--- webApp/models.py
+++ webApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class video(models.Model):
+    channel_title = models.CharField(max_length = 200, null=True, blank=True)
     video_title = models.CharField(max_length = 200)
     video_description = models.TextField()
     date_time = models.DateTimeField()
--- webApp/views.py
+++ webApp/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 import requests
-from rest_framework.response import Response
 from django.http import JsonResponse
-from rest_framework import status
+from .models import video
 import json
-from apscheduler.schedulers.background import BackgroundScheduler
+
 
 def show_latest_videos(request):
+    # collecting the response from the Youtube API
+    # search query = official
     response = requests.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=date&q=official&key=AIzaSyDCa32_gehikY8wO2EOm3kYStQE27aGXhA')
     
     if response.status_code == 200:
         response = response.json()["items"]
         # return JsonResponse(response, safe=False)
 
+        # Storing Video Details in the Database(video Model)
+        for v in response:
+            try:
+                title = v.snippet.title
+                desc = v.snippet.description
+                dt = v.snippet.publishedAt
+                url = v.snippet.thumbnails.medium.url
+                channel = v.snippet.channelTitle
+
+                obj = video(channel_title = channel, video_title = title, video_description = desc, date_time = dt, thumbnail_url = url)
+                obj.save()
+            except:
+                continue
+
+        # return index page with response to show videos in chronological order with paginated response
         return render(request, 'main/index.html', {'response': response})
     else:
         return render(request, 'main/index.html', {'response': response})
