cacd72f8262db2de8133dba521436a2dfc91b26c
unknown <vaibhav6644@gmail.com>
2021-12-19 15:58:50 +0530

    Videos are loading in reverse chronological order, async background calls part is left

--- webApp/views.py
+++ webApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import requests
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
+import json
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# Create your views here.
+
+def show_latest_videos(request):
+    response = requests.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=date&q=official&key=AIzaSyDOU19eHjBasnNAlDamvnhNGWLc4BdWrrQ')
+    try:
+        if response.status_code == 200:
+            response = response.json()
+            return JsonResponse(response, safe=False)
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+
+
+# def start(request):
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(show_latest_videos, 'interval', seconds=10)
+#     scheduler.start()
