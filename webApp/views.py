960d81c29f77bb63e30b50690a1e3205d31db083
unknown <vaibhav6644@gmail.com>
2021-12-21 18:52:17 +0530

    Feature added - Server calling the Youtube API continuously with interval of 10 seconds for fetching latest videos for predefined search query official and also storing the data in database

--- webApp/views.py
+++ webApp/views.py
@@ -8,29 +8,39 @@ import json
 def show_latest_videos(request):
     # collecting the response from the Youtube API
     # search query = official
-    response = requests.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&q=official&key=AIzaSyAR8YCmbh_qMwI6-Ke1INSgLeYWUs2oUt0')
+
+    search_url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&q=official&key=AIzaSyAR8YCmbh_qMwI6-Ke1INSgLeYWUs2oUt0'
+    response = requests.get(search_url) # Collecting the response from the Youtube API
     
     if response.status_code == 200:
         response = response.json()["items"] # response contains the search query result in a JSON Object
-        
+
+        # Storing Video Details in the Database(video Model)
+        n = len(response)
+        for i in range(n):
+            try:
+                title = response[i]["snippet"]["title"]
+                desc = response[i]["snippet"]["description"]
+                dt = response[i]["snippet"]["publishedAt"]
+                url = response[i]["snippet"]["thumbnails"]["medium"]["url"]
+                channel = response[i]["snippet"]["channelTitle"]
+
+                obj = video(
+                    channel_title = channel, 
+                    video_title = title, 
+                    video_description = desc, 
+                    date_time = dt, 
+                    thumbnail_url = url)
+                obj.save()
+            except:
+                continue
+
+
         # if you want to run the API in PostMan, uncomment the line belowJsonResponse(response, safe=False) Hand
         # comment down the lines after the below line, since PostMan requires a JSON Object. 
         
         # return JsonResponse(response, safe=False)
 
-        # Storing Video Details in the Database(video Model)
-        for v in response:
-            try:
-                title = v.snippet.title
-                desc = v.snippet.description
-                dt = v.snippet.publishedAt
-                url = v.snippet.thumbnails.medium.url
-                channel = v.snippet.channelTitle
-
-                obj = video(channel_title = channel, video_title = title, video_description = desc, date_time = dt, thumbnail_url = url)
-                obj.save()
-            except:
-                continue
 
         # return index page with response to show videos in chronological order with paginated response
         return render(request, 'main/index.html', {'response': response})
