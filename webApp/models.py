4c9fd9a2955c8bc62cfff12773a8752fa6e42a59
unknown <vaibhav6644@gmail.com>
2021-12-22 17:54:52 +0530

    CSS updated for Paginated Response of Videos and comments added.

--- webApp/models.py
+++ webApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class video(models.Model):
+    index = models.IntegerField()
     channel_title = models.CharField(max_length = 200, null=True, blank=True)
     video_title = models.CharField(max_length = 200)
     video_description = models.TextField()
--- webApp/views.py
+++ webApp/views.py
@@ -13,27 +13,25 @@ async def fetch(client, url):
 
 # search query = official
 async def show_latest_videos(request):
-    # collecting the response from the Youtube API with asyncio
 
+    # collecting the response from the Youtube API with asyncio
     search_url = ['https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&q=official&key=AIzaSyAR8YCmbh_qMwI6-Ke1INSgLeYWUs2oUt0']
-    
     async with aiohttp.ClientSession() as client:
         tasks = []
-        for url in search_url:
-            task = asyncio.ensure_future(fetch(client, url))
-            tasks.append(task)
+        task = asyncio.ensure_future(fetch(client, search_url[0]))
+        tasks.append(task)
 
-        qs = await asyncio.gather(*tasks)
+        qs = await asyncio.gather(*tasks) # qs contains the search query result in a JSON Object
 
     
-    response = qs[0]["items"] # response contains the search query result in a JSON Object
+    response = qs[0]["items"] # fetching video details from JSON object as items which is returning a List
 
-    store_videos_db(response)
+    await store_videos_db(response) # storing video details in database
 
     # if you want to run the API in PostMan, uncomment the line belowJsonResponse(response, safe=False) Hand
     # comment down the lines after the below line, since PostMan requires a JSON Object. 
-        
     # return JsonResponse(response, safe=False)
+
 
     # return index page with response to show videos in chronological order with paginated response
     return render(request, 'main/index.html', {'response': response})
@@ -41,8 +39,15 @@ async def show_latest_videos(request):
 @sync_to_async
 def store_videos_db(response):
     # Storing Video Details in the Database(video Model)
+
+    if len(video.objects.all()) != 0:
+        index = len(video.objects.all())
+    else:
+        index = 0
+
     n = len(response)
     for i in range(n):
+        index = index + 1
         title = response[i]["snippet"]["title"]
         desc = response[i]["snippet"]["description"]
         dt = response[i]["snippet"]["publishedAt"]
@@ -50,6 +55,7 @@ def store_videos_db(response):
         channel = response[i]["snippet"]["channelTitle"]
 
         obj = video(
+            index = index,
             channel_title = channel, 
             video_title = title, 
             video_description = desc, 
